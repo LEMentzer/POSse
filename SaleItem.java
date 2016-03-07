a461263d946e126cb92f46a0832785f0ab06575f
Cynthia Fevola <Cynthia Fevola>
2016-03-07 16:51:40 -0500

    First version of Item and SaleItem

---
+++ SaleItem.java
@@ -0,0 +1,27 @@
+class SaleItem {
+  private Item item;
+  private int quantity;
+  private double total = 0;
+  
+  SaleItem(Item item, int quantity, double total) {
+    this.item = item;
+    this.quantity = quantity;
+    this.total = total;
+  }
+  
+  Item getItem() {
+    return item;
+  }
+  
+  int getQuantity() {
+    return quantity;
+  }
+  
+  double getTotal() {
+    return total;
+  }
+  
+  void updateQuantity(int quantity) {
+    this.quantity += quantity;
+  }
+}
