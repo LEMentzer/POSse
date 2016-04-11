78e462cff0dd7581720f6d6dc6fa323536626d82
dag217 <dag217@lehigh.edu>
2016-04-11 18:48:59 -0400

    Added Receipt functionality

--- POSse/src/posse/Transaction.java
+++ POSse/src/posse/Transaction.java
@@ -6,13 +6,12 @@ public abstract class Transaction{
   double total;
   double subtotal;
   double tax;
-  String receipt;
   
   abstract AbstractItem addItem(int input, int quantity) throws SQLException;
   abstract Boolean removeItem(int input, int quantity) throws SQLException;
   abstract double calculateTotal();
   abstract double calculateSubtotal();
   abstract double calculateTax();
-  abstract String printReceipt();
+  abstract Receipt printReceipt();
   
 }
