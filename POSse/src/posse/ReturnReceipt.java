2bccf65fb586955cbc5e1289dd0556f45427e226
Irene Lau <irl216@lehigh.edu>
2016-04-11 18:49:31 -0400

    Update ReturnReceipt.java

--- POSse/src/posse/ReturnReceipt.java
+++ POSse/src/posse/ReturnReceipt.java
@@ -8,20 +8,16 @@ public class ReturnReceipt extends Receipt{
   private double tax;
   private double subtotal;
   
-  SaleReceipt(ArrayList<SaleItem> returns,double total, double tax, double subtotal){
+  ReturnReceipt(ArrayList<ReturnItem> returns,double total, double tax, double subtotal){
     this.returns = returns;
     this.total = total;
     this.tax = tax;
     this.subtotal = subtotal;
-    text = makeText(returns);
-  }
-  
-  private static makeText(returns){
     StringBuilder sb = new StringBuilder();
     String s = "Return Receipt:\n\n";
-    sb.append(begin);
+    sb.append(s);
     for(int i = 0; i < returns.size(); i++){
-      Item = returns.get(i).getItem();
+      Item item = returns.get(i).getItem();
       sb.append(item.getItemID() + "\t");
       sb.append(item.getName() + "\t");
       sb.append(returns.get(i).getQuantity());
