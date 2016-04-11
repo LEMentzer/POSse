e18ef5bde79c31f34a3706921a3d20199907d20e
zhl216 <zhuobths@gmail.com>
2016-04-10 22:14:26 -0400

    Added files via upload

---
+++ POSse/src/posse/RentalItem.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posse;
+
+/**
+ *
+ * @author Zhuo
+ */
+public class RentalItem {
+  private Item item;
+  private int quantity;
+  private double total = 0;
+  
+  RentalItem(Item item, int quantity, double total) {
+    this.item = item;
+    this.quantity = quantity;
+    this.total = total;
+  }
+  
+  Item getItem() {
+    return item;
+  }
+  
+  int getQuantity() {
+    return quantity;
+  }
+  
+  double getTotal() {
+    return total;
+  }
+  
+  void updateQuantity(int quantity) {
+    this.quantity += quantity;
+  }
+}
