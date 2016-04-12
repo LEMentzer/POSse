380154de946701646656a93e11fd9644f6ab3724
dag217 <dag217@lehigh.edu>
2016-04-12 17:45:17 -0400

    Fixed receipt shenanigans

--- POSse/src/posse/RentalReceipt.java
+++ POSse/src/posse/RentalReceipt.java
@@ -1,5 +1,6 @@
 package posse;
 import java.util.ArrayList;
+import java.text.DecimalFormat;
 
 public class RentalReceipt extends Receipt{
   private ArrayList<RentalItem> rentals;
@@ -16,19 +17,22 @@ public class RentalReceipt extends Receipt{
     StringBuilder sb = new StringBuilder();
     String s = "Rental Receipt:\n\n";
     sb.append(s);
+    DecimalFormat f = new DecimalFormat("##.00");
     for(int i = 0; i < rentals.size(); i++){
       Item item = rentals.get(i).getItem();
       sb.append(item.getItemID() + "\t");
       sb.append(item.getName() + "\t");
-      sb.append(rentals.get(i).getQuantity());
-      sb.append(rentals.get(i).getReturnDate());
+      sb.append(rentals.get(i).getQuantity() + "\t");
+      sb.append("$" + f.format(rentals.get(i).getTotal()) + "\t");
+      sb.append("Return by: " + rentals.get(i).getReturnDate());
+      sb.append("\n");
     }
     
     text = sb.toString();
     
-    text += ("\n\nSubtotal: " + "$" + subtotal);
-    text += ("\nTax: " + "$" + tax);
-    text += ("\nTotal: " + "$" + total);
+    text += ("\n\nSubtotal: " + "$" + f.format(subtotal));
+    text += ("\nTax: " + "$" + f.format(tax));
+    text += ("\nTotal: " + "$" + f.format(total));
   }
   
   public String getText(){
--- POSse/src/posse/ReturnReceipt.java
+++ POSse/src/posse/ReturnReceipt.java
@@ -1,4 +1,5 @@
 package posse;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class ReturnReceipt extends Receipt{
@@ -16,18 +17,21 @@ public class ReturnReceipt extends Receipt{
     StringBuilder sb = new StringBuilder();
     String s = "Return Receipt:\n\n";
     sb.append(s);
+    DecimalFormat f = new DecimalFormat("##.00");
     for(int i = 0; i < returns.size(); i++){
       Item item = returns.get(i).getItem();
       sb.append(item.getItemID() + "\t");
       sb.append(item.getName() + "\t");
-      sb.append(returns.get(i).getQuantity());
+      sb.append(returns.get(i).getQuantity() + "\t");
+      sb.append("$" + f.format(returns.get(i).getTotal()));
+      sb.append("\n");
     }
     
     text = sb.toString();
     
-    text += ("\n\nSubtotal: " + "$" + subtotal);
-    text += ("\nTax: " + "$" + tax);
-    text += ("\nTotal: " + "$" + total);
+    text += ("\n\nSubtotal: " + "$" + f.format(subtotal));
+    text += ("\nTax: " + "$" + f.format(tax));
+    text += ("\nTotal: " + "$" + f.format(total));
   }
   
   public String getText(){
