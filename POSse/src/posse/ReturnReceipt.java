e4667e881331147a1ffa2f85ca088cdbba705bde
dag217 <dag217@lehigh.edu>
2016-04-26 21:31:58 -0400

    Made formatting nicer

--- POSse/src/posse/ReturnReceipt.java
+++ POSse/src/posse/ReturnReceipt.java
@@ -18,17 +18,24 @@ public class ReturnReceipt extends Receipt{
     String s = "Return Receipt:\n\n";
     sb.append(s);
     DecimalFormat f = new DecimalFormat("##.00");
-    for(int i = 0; i < returns.size(); i++){
+    /*for(int i = 0; i < returns.size(); i++){
       Item item = returns.get(i).getItem();
       sb.append(item.getItemID() + "\t");
       sb.append(item.getName() + "\t");
       sb.append(returns.get(i).getQuantity() + "\t");
       sb.append("$" + f.format(returns.get(i).getTotal()));
       sb.append("\n");
+    }*/
+    
+    for(int i = 0; i < returns.size(); i++){
+      Item item = returns.get(i).getItem();
+      s += String.format("%-8s", String.valueOf(item.getItemID()));
+      s += String.format("%-50s", item.getName());
+      s += String.format("%-6s", String.valueOf(returns.get(i).getQuantity()));
+      s += ("$" + f.format(returns.get(i).getTotal()) + "\n");
     }
     
-    text = sb.toString();
-    
+    text = s + "\n\n";
     text += ("\n\nSubtotal: " + "$" + f.format(subtotal));
     text += ("\nTax: " + "$" + f.format(tax));
     text += ("\nTotal: " + "$" + f.format(total));
